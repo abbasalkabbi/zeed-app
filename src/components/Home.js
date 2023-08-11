79c5e3035a2008769c7474ff0eb50388ca58d09e
abbas_alkaabi <abbasalkabbi2003@gmail.com>
2023-08-11 06:38:30 +0300

    "Add =>slider-area in Home"

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import  image from "../assets/slider-left-img.png"
+import  slider_left_img from "../assets/slider-left-img.png"
 
 function Home(){
     return(
@@ -7,9 +7,15 @@ function Home(){
             <div className="slider-area">
                 {/* slider-img */}
                 <div className="slider-img">
-                    <img src={image}/>
+                    <img src={slider_left_img} alt="slider_left_img"/>
                 </div>
                 {/* slider-img End */}
+                {/* slider-inner */}
+                <div className="slider-inner">
+                    <h2>Who Else Wants To User</h2>
+                    <h5>And Use Our Zeed App !</h5>
+                </div>
+                {/* slider-inner End */}
             </div>
             {/* slider-area End */}
         </div>
