1b2583f31bbf38012fba464a29a2a19f1cb0829c
abbas_alkaabi <abbasalkabbi2003@gmail.com>
2023-08-15 02:50:10 +0300

    "Add -> Footer"

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Features from "./components/Features";
 import Pricing from "./components/Pricing";
 import Download from "./components/Download";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Pricing/>
       <Download/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+    <div className="Footer">
+    <p>Copyright ©2023 All rights reserved | This template is made with  by <a href="https://preview.colorlib.com/theme/zeedapp/zeedapp/index.html" >Colorlib</a></p>
+    <p>Develop by <a href="https://github.com/abbasalkabbi/zeed-app" >abbasalkabbi</a></p>
+    </div>
+    )
+}
+export default Footer;
